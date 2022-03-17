554e978e9f1db0c9f4605f8ac69d465da03a23ca
DariusPirvulescu <organicdarius@gmail.com>
2022-03-17 20:13:01 +0000

    Install and configure Flask Mail, add mailing route

--- app.py
+++ app.py
@@ -3,9 +3,20 @@ import os
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
+from flask_mail import Mail, Message
 
 # Configure application
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+
+# Configure Falsk Mail
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config.from_pyfile('config.py')
+
+# Initiate mail from Flask Mail
+mail = Mail(app)
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -22,12 +33,10 @@ db = SQL("sqlite:///dog.db")
 def index():
   return render_template("index.html")
 
-# CREATE TABLE users (
-#   id INTEGER, 
-#   email TEXT NOT NULL, 
-#   username TEXT NOT NULL, 
-#   profile_photo_url TEXT, 
-#   Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, 
-#   bio TEXT,
-#   PRIMARY KEY(id)
-# )
+@app.route("/mailing/<mail_address>")
+def mailing(mail_address):
+   """Send a template mail"""
+   msg = Message('Hello', sender = 'securedog@gmail.com', recipients = [str(mail_address)])
+   msg.body = "Secure dog email"
+   mail.send(msg)
+   return "Sent"
