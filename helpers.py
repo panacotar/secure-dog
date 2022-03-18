839ac015aed66bc2f6ab1ccfac8207ddf90b1f69
DariusPirvulescu <organicdarius@gmail.com>
2022-03-18 18:41:59 +0000

    Add get_expiration_date_milliseconds helper

--- helpers.py
+++ helpers.py
@@ -14,3 +14,20 @@ def login_required(f):
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
+
+def get_confirmation_code():
+  code = ""
+  random_chars = [randint(0, 9) for _ in range(6)]
+  return code.join(str(x) for x in random_chars)
+
+def get_time_now_ms():
+  return int(time.time()) * 1000
+
+def get_expiration_date_milliseconds():
+  # Get 15 mins in ms
+  unix_15 = 900000
+  # Get time now in ms
+  time_now_ms = get_time_now_ms()
+  # Return the unix time 15 mins from now
+  return time_now_ms + unix_15
+
