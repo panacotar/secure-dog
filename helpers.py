500f17056482bab3b9cf32e77bb8e90fb824d3e6
DariusPirvulescu <organicdarius@gmail.com>
2022-03-18 18:41:41 +0000

    Add login_required decorator

--- helpers.py
+++ helpers.py
@@ -1,6 +1,16 @@
 from random import randint
+import time
+from flask import redirect, session
+from functools import wraps
 
-def get_confirmation_code():
-  code = ""
-  random_chars = [randint(0, 9) for _ in range(6)]
-  return code.join(str(x) for x in random_chars)
+def login_required(f):
+    """
+    Decorate routes to require login.
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
